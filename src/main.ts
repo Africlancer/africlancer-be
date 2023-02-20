b4d36e393368def7b34a863843546bf095edd302
Sabi Ridwan <sabiridwan.dev@gmail.com>
2023-02-20 22:07:33 +0800

    update payload limit;

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({ origin: true, credentials: true });
   app.useGlobalPipes(new ValidationPipe())
+  app.use(express.json({ limit: "50mb" }));
+  app.use(express.urlencoded({ limit: "50mb", extended: true }));
   await app.listen(process.env.port);
   console.log(`app started at http://localhost:${process.env.port}`)
 }
