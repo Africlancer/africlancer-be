3d00d386a5d6c55f2ed81060c85762c88d3366e5
Sabi Ridwan <sabiridwan.dev@gmail.com>
2023-02-20 21:29:28 +0800

    user profile endpoint;

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { User } from './user.schema';
 import { UserService } from './user.service';
 
@@ -6,28 +16,22 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userSvc: UserService) {}
 
-  @Post()
-  public create(@Body() user: User): Promise<User> {
-    return this.userSvc.create(user);
-  }
+  @Get('retrieve-by-app/:id/:token')
+  public async findOne(
+    @Param('id') id: string,
+    @Param('token') token: string,
+  ): Promise<User> {
+    if (token !== process.env.AP_ACCESS_TOKEN)
+      throw new HttpException('Invalid access', HttpStatus.FORBIDDEN);
+    const rs = await this.userSvc.findOne({ _id: id } as any);
 
-  @Put()
-  public async update(_id: string, user: User): Promise<void> {
-    this.userSvc.update(_id, user);
-  }
-
-  @Get('retrieve/:id')
-  public async findOne(@Param() id: string): Promise<User> {
-    return this.userSvc.findOne({ _id: id } as any);
-  }
-
-  @Get('/all')
-  public async find(): Promise<User[]> {
-    return this.userSvc.find({} as any);
-  }
-
-  @Delete(':id')
-  public async delete(@Param() id: string): Promise<void> {
-    await this.userSvc.delete(id);
+    return {
+      _id: rs._id,
+      email: rs.email,
+      username: rs.username,
+      firstName: rs.firstName,
+      lastName: rs.lastName,
+      roles: rs.roles,
+    } as User;
   }
 }
