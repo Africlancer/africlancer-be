a652317a91cb1aeb333000aabf3aff62765e8330
thisisazeez <thisisazeez0@gmail.com>
2023-04-06 14:33:37 +0100

    notification module working with subscription

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { APP_GUARD } from '@nestjs/core';
 import { GqlRolesGuard } from './modules/auth/guards/gql.roles.guard';
 import { MailModule } from './modules/mail/mail.module';
 import { BidModule } from './modules/bid/bid.module';
-import { MessagesModule } from './messages/messages.module';
+// import { MessagesModule } from './modules/messages/messages.module';
 
 @Module({
   imports: [
@@ -39,8 +39,16 @@ import { MessagesModule } from './messages/messages.module';
       cors: { origin: true, credentials: true },
       context: ({ req, res }) => ({ req, res }),
       //subcription
+      installSubscriptionHandlers: true,
       subscriptions: {
         'graphql-ws': true,
+        'subscriptions-transport-ws': true,
+        // path: '/subscriptions',
+        // transport: 'websocket',
+        // wsOptions: {
+        //   maxPayload: 100 * 1024 * 1024, // 100MB
+        //   perMessageDeflate: true,
+        // },
       },
     }),
     MongooseModule.forRoot(`mongodb://localhost/${process.env.DB_NAME}`),
@@ -54,7 +62,6 @@ import { MessagesModule } from './messages/messages.module';
     BidModule,
     NotificationModule,
     PubsubModule,
-    MessagesModule,
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: GqlRolesGuard }],
--- src/modules/projects/project.resolver.ts
+++ src/modules/projects/project.resolver.ts
@@ -8,6 +8,7 @@ import { GqlJwtGuard } from '../auth/guards/gql.jwt.guard';
 import { Role } from '../auth/roles.enum';
 import { PUB_SUB } from '../pubsub/pubsub.module';
 import { RedisPubSub } from 'graphql-redis-subscriptions';
+
 import {
   CommonProjectInput,
   CreateProjectInput,
@@ -21,16 +22,21 @@ import { ProjectService } from './project.service';
 enum SUBSCRIPTION_EVENTS{
   newProject = 'newProject',
 }
-
+// }export class MyClass {
+//   allSubscribers: Subscription;
+// }
 @Resolver((of) => Project)
 export class ProjectResolver {
   constructor(@Inject(PUB_SUB) private readonly pubSub: RedisPubSub, private readonly projectService: ProjectService, @InjectMapper() private readonly classMapper: Mapper) {}
+
+
 
   // @Resolver()
   // export class ProjectResolver {
     allSubscribers: Project[] = []
     // constructor (@inject(PUB_SUB) private readonly pubSub: RedisPubSub){}
   // @Mutation()
+  
   //   CreateProjectInput(@Args("project") project: Project){
 
       
@@ -44,12 +50,12 @@ export class ProjectResolver {
 
 
   @Mutation((returns) => Project, { name: 'createProject' })
-  @UseGuards(GqlJwtGuard)
-  @Roles(Role.USER)
+  // @UseGuards(GqlJwtGuard)
+  // @Roles(Role.USER)
   public async create(@GqlCurrentUser() user:any, @Args('project') project: CreateProjectInput): Promise<Project> {
-    project.userId = user.sub;
+    // project.userId = user.sub;
     const queryMap = await this.classMapper.mapAsync(project, CreateProjectInput, ProjectSchema);
-    // edit next line to any if it doesnt run - azeezSaid
+    // // edit next line to any if it doesnt run - azeezSaid
     this.allSubscribers.push(new Project)
     this.pubSub.publish(SUBSCRIPTION_EVENTS.newProject, {newProject: project})
     return this.classMapper.mapAsync(await this.projectService.create(queryMap), ProjectSchema, Project);
