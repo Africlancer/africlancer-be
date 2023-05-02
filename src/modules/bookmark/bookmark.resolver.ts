7de8573db4e2184c077f597ef8cff9e55885d148
gifteebaby <103193391+gifteebaby@users.noreply.github.com>
2023-05-02 12:18:03 +0100

    updated bookmark module

--- src/modules/bookmark/bookmark.resolver.ts
+++ src/modules/bookmark/bookmark.resolver.ts
@@ -4,9 +4,7 @@ import { Param } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import {
     CreateBookmarkInput,
-    UpdateBookmarkInput,
     Bookmark,
-    BookmarkQueryInput,
     QueryBookMarkInput,
 } from './bookmark.model';
 import { BookMark as BookmarkSchema } from './bookmark.schema';
@@ -18,14 +16,14 @@ export class BookmarkResolver {
     constructor(private readonly BookmarkService: BookmarkService, @InjectMapper() private readonly classMapper: Mapper) { }
 
     @Mutation((returns) => Bookmark, { name: 'createBookmark' })
-    public async create(@Args('portfolio') Bookmark: CreateBookmarkInput): Promise<Bookmark> {
+    public async create(@Args('bookmark') Bookmark: CreateBookmarkInput): Promise<Bookmark> {
         return (await this.BookmarkService.create(Bookmark as any)) as any;
     }
 
     @Mutation((returns) => Boolean, { name: 'updateBookmark' })
     public async update(
         @Args('_id') _id: string,
-        @Args('Bookmark') bookmark: UpdateBookmarkInput,
+        @Args('bookmark') bookmark: QueryBookMarkInput,
     ): Promise<boolean> {
         // const queryMap = await this.classMapper.mapAsync(bookmark, UpdateBookmarkInput, BookmarkSchema)
         await this.BookmarkService.updateOne(_id, bookmark as any);
@@ -33,7 +31,7 @@ export class BookmarkResolver {
     }
     @Query((returns) => [Bookmark], { name: 'findBookmark' })
     //    public async find(@Args('query') query: BookmarkQueryInput): Promise<Bookmark[]> 
-    public async find(@Args('query') query: QueryBookMarkInput): Promise<Bookmark[]> {
+    public async find(@Args('bookmark') query: QueryBookMarkInput): Promise<Bookmark[]> {
         return (await this.BookmarkService.find(query as any)) as any;
     }
 
