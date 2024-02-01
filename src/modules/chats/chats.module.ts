2c85042ecb9fcbfa444b03a0cc955e612fd7c107
thisisazeez <thisisazeez0@gmail.com>
2024-02-01 13:02:48 +0100

    chat module

--- src/modules/chats/chats.module.ts
+++ src/modules/chats/chats.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ChatsController } from './chats.controller';
+import { ChatsService } from './chats.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/modules/user/user.schema';
+import { AuthService } from 'src/modules/auth/auth.service';
+import { UserService } from 'src/modules/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { Message, MessageSchema } from './message.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [ChatsController],
+  providers: [ChatsService, AuthService, UserService, JwtService],
+})
 export class ChatsModule {}
